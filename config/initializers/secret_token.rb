905e07054bb50c3e85b959fc8c935a7a0c201102
mike <mttownsend1@gmail.com>
2012-07-24 21:49:54 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bling::Application.config.secret_token = '7ee33144ce936044ce1dc48a8d522d89bf77a2f4fdceff55a74936aaca3793206c5b2c96c350d391cd60df1a9543d8b4244c034fa1db1ffb17294e6b1aa69105'
